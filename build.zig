8357f5ec3c080d59e6fce9be53b33863d8868236
Raúl Wolters <56522208+smups@users.noreply.github.com>
2024-08-06 10:25:49 +0200

    updated build script for z0.13

--- build.zig
+++ build.zig
@@ -2,13 +2,6 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 
  pub fn build(b: *std.Build) !void {
-    // Initialise allocator
-    var arena_state = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena_state.deinit();
-    const alloc = arena_state.allocator();
-
-    const root_dir = b.build_root.handle;
-
     // Get user-supplied target and optimize functions
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
@@ -24,30 +17,46 @@ const Allocator = std.mem.Allocator;
     yaml_cpp.linkLibCpp();
 
     // Include headers
-    yaml_cpp.addIncludePath(.{ .path = "include" });
+    yaml_cpp.addIncludePath(b.path("include"));
 
     // Add source files
     const cpp_flags = &.{"-std=c++11"};
-    const cpp_src = try list_cpp_src(alloc, try root_dir.openDir("src", .{}));
-    yaml_cpp.addCSourceFiles(cpp_src.items, cpp_flags);
+    const cpp_src = &.{
+        "src/binary.cpp",
+        "src/convert.cpp",
+        "src/depthguard.cpp",
+        "src/directives.cpp",
+        "src/emit.cpp",
+        "src/emitfromevents.cpp",
+        "src/emitter.cpp",
+        "src/emitterstate.cpp",
+        "src/emitterutils.cpp",
+        "src/exceptions.cpp",
+        "src/exp.cpp",
+        "src/memory.cpp",
+        "src/nodebuilder.cpp",
+        "src/node.cpp",
+        "src/node_data.cpp",
+        "src/nodeevents.cpp",
+        "src/null.cpp",
+        "src/ostream_wrapper.cpp",
+        "src/parse.cpp",
+        "src/parser.cpp",
+        "src/regex_yaml.cpp",
+        "src/scanner.cpp",
+        "src/scanscalar.cpp",
+        "src/scantag.cpp",
+        "src/scantoken.cpp",
+        "src/simplekey.cpp",
+        "src/singledocparser.cpp",
+        "src/stream.cpp",
+        "src/tag.cpp"
+    };
+    yaml_cpp.addCSourceFiles(.{ .files = cpp_src, .flags = cpp_flags });
 
     // Add headers to install directory
-    yaml_cpp.installHeadersDirectory("include", "");
+    yaml_cpp.installHeadersDirectory(b.path("include"), "", .{});
 
     // Install headers + binary
     b.installArtifact(yaml_cpp);
-}
-
-/// This function traverses the `src_dir` and produces an `ArrayList` of all
-/// non-main source files in the `src_dir`.
-fn list_cpp_src(alloc: Allocator, src_dir: std.fs.Dir) !std.ArrayList([]u8) {
-    var source_files = std.ArrayList([]u8).init(alloc);
-    var walker = (try src_dir.openIterableDir(".", .{})).iterate();
-    while (try walker.next()) |entry| {
-        if (!std.mem.endsWith(u8, entry.name, ".cpp")) {
-            continue;
-        }
-        try source_files.append(try src_dir.realpathAlloc(alloc, entry.name));
-    }
-    return source_files;
 }
